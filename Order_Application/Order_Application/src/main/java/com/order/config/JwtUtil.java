fa584b800f5f3957612502f55bb34cec0e8f8cc1
unknown <sps783804@gmail.com>
2026-02-05 13:24:15 +0530

    I commited upto date

--- Order_Application/Order_Application/src/main/java/com/order/config/JwtUtil.java
+++ Order_Application/Order_Application/src/main/java/com/order/config/JwtUtil.java
@@ -3,14 +3,15 @@ package com.order.config;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-
-    private final String SECRET_KEY = "my-secret-key-my-secret-key";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY ;
 
     public String generateToken(String username) {
         return Jwts.builder()
